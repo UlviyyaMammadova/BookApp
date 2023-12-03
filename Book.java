63991a91dc94be32454166e2f6ca06500468f057
Ulviyya Mammadova <113963177+UlviyyaMammadova@users.noreply.github.com>
2023-12-03 19:27:47 +0400

    Getters and Setters

---
+++ Book.java
@@ -0,0 +1,54 @@
+public class Book {
+    private int bookId;
+    private String title;
+    private int stockQuantity;
+    private int authorId;
+
+    public Book(int bookId, String title, int stockQuantity, int authorId) {
+        this.bookId = bookId;
+        this.title = title;
+        this.stockQuantity = stockQuantity;
+        this.authorId = authorId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", title='" + title + '\'' +
+                ", stockQuantity=" + stockQuantity +
+                ", authorId=" + authorId +
+                '}';
+    }
+}

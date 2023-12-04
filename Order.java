f987b83e0d6a62d893c8f37b4dffd442eb8b65f4
Ulviyya Mammadova <113963177+UlviyyaMammadova@users.noreply.github.com>
2023-12-04 11:28:01 +0400

    Author, customer and order

---
+++ Order.java
@@ -0,0 +1,50 @@
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    private int orderId;
+    private Date orderDate;
+    private int bookId;
+    private int customerId;
+    private List<Book> books;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
